dd46d698af46b94689aeb95a69b29e7170334269
Marufjon Khaliev <marufjon1991mike@gmail.com>
2026-05-18 04:01:01 +0500

    Add files via upload

---
+++ python-lesson-03-print().py
@@ -0,0 +1,25 @@
+# Quyidagi matnni aynan shunday ko'rinishda konsolda chiqaring:
+
+# "Nexia", "Tico", 'Damas' ko'rganlar qilar havas
+print(f'"Nexia", "Tico", \'Damas\', ko\'rganlar qilar havas ')
+
+
+
+# Quyidagi misollarga yechimni Pythonda chiqaring. Har bir misoldan avval misol matnini izoh ko'rinishida yozing:
+
+# 5 ning 4-darajasini toping
+print(f"5 ning 4-darajasi {5**4} ga teng")
+
+# 22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+print(f"22 ni 4 ga bo'lganda {22%4} qoldiq qoladi")
+
+# Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+print(f"Tomonlari 125 ga teng kvadratning yuzi {125**2}, perimetri {125*4} ga teng" )
+
+# Diametri 12 ga teng bo'lgan doiraning yuzini toping (  𝜋=3.14  deb oling)
+print(f"Diametri 12 ga teng bo'lgan doiraning yuzi {(12/2)**2*3.14} ga teng")
+
+# Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor teoremasidan foydalaning)
+print(f"Katetlari 6 vga 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi {(6**2+7**2)**0.5}")
+
+# !! Javoblarni savol va yechim ko'rinishida chiqaring: 5 ning 4-darajasi 625 !!
