89bfbe24caff04b14b46a8b276afcab4abe6132c
Marufjon Khaliev <marufjon1991mike@gmail.com>
2026-05-18 04:12:03 +0500

    print()

--- python-lesson-03-print().py
+++ python-lesson-03-print().py
@@ -1,9 +1,7 @@
 # Quyidagi matnni aynan shunday ko'rinishda konsolda chiqaring:
 
 # "Nexia", "Tico", 'Damas' ko'rganlar qilar havas
-print(f'"Nexia", "Tico", \'Damas\', ko\'rganlar qilar havas ')
-
-
+print(f'"Nexia", "Tico", \'Damas\', ko\'rganlar qilar havas')
 
 # Quyidagi misollarga yechimni Pythonda chiqaring. Har bir misoldan avval misol matnini izoh ko'rinishida yozing:
 
@@ -23,3 +21,4 @@ print(f"Diametri 12 ga teng bo'lgan doiraning yuzi {(12/2)**2*3.14} ga teng")
 print(f"Katetlari 6 vga 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi {(6**2+7**2)**0.5}")
 
 # !! Javoblarni savol va yechim ko'rinishida chiqaring: 5 ning 4-darajasi 625 !!
+print(f"test")
