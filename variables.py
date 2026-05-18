577e409c4547cb52632d1db2848a485c22755ba0
Marufjon Khaliev <marufjon1991mike@gmail.com>
2026-05-19 03:28:37 +0500

    variables

---
+++ variables.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 03:21:32 2026
+
+@author: khalievmarufjon
+"""
+
+# "Hello World!" matnini yangi o'zgaruvchiga yuklang va print() 
+# yordamida konsolga chiqaring
+test = "Hello world"
+print(test)
+
+# xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga 
+# chiqaring, keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga 
+# chiqaring.
+xabar = "Bu xabar"
+print(xabar)
+xabar = "Bu yangi xabar"
+print(xabar)
+
+# class den nomlangan o'zgaruvchi yarating, unga biror qiymat bering 
+# va konsolga chiqaring (siz kutgan natija chiqdimi?) 
+# class = "bu klass"
+# print(class)
+
+# Quyidagi kodni bajaring:
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
